c9bb8be67099c4f6186b05ff0fd21a021846d4ed
Ankit Kumar Yadav <ankit705yadav@gmail.com>
2023-10-13 18:00:21 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import { toast } from "react-toastify";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://full-stack-real-estate-317wye87w-ankit705yadav.vercel.app/api",
 });
 
 // https://full-stack-real-estate-317wye87w-ankit705yadav.vercel.app/
